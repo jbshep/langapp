e610c4abafdc07170c810b3fde9472f8522b174d
jbshep <jbshep@gmail.com>
2025-02-21 09:33:12 -0600

    i18n setup

---
+++ i18n.py
@@ -0,0 +1,15 @@
+langs = ["en_US", "es_MX"]
+lang = "en_US"
+
+t = {
+    "en_US" : {
+        "name" : "name",
+        "hello" : "hello",
+        "world" : "world",
+    }
+    "es_MX" : {
+        "name" : "nombre",
+        "hello" : "hola",
+        "world" : "mundo",
+    }
+}
